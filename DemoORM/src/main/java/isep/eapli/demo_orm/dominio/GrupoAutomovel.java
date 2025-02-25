3056beebbea3a523d776e60d2030536e1929bed4
helen <1230896@isep.ipp.pt>
2025-02-25 16:40:22 +0000

    Criação do método setnPortas da Classe GrupoAutomovel .

--- DemoORM/src/main/java/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
+++ DemoORM/src/main/java/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
@@ -1,4 +1,9 @@
 package isep.eapli.demo_orm.dominio;
 
 public class GrupoAutomovel {
+    Integer nPortas;
+
+    public void setnPortas(Integer nPortas) {
+        this.nPortas = nPortas;
+    }
 }
