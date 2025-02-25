a775568531a783e95ce1e77603ac41410a679117
helen <1230896@isep.ipp.pt>
2025-02-25 16:25:06 +0000

    Método main implementado

--- DemoORM/src/main/java/isep/eapli/demo_orm/DemoORM.java
+++ DemoORM/src/main/java/isep/eapli/demo_orm/DemoORM.java
@@ -2,6 +2,6 @@ package isep.eapli.demo_orm;
 
 public class DemoORM {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       MainMenu.mainLoop();
     }
 }
